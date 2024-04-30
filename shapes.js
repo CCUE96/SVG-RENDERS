023ebc7b92bf98a7fb5e0dd9bd2750f2109077a3
Christopher Cuellar <rival100x@gmail.com>
2024-04-30 16:34:59 -0400

    fixed a syntax error with my triangle method

--- shapes.js
+++ shapes.js
@@ -32,7 +32,7 @@ class Triangle extends Shape{
     render(){
         return `<svg width="100" height="100">
         <polygon points="50,0 100,100 0,100" fill="${this.color}"/>
-        <text x="50" y="50" text-anchor="middle" dominant-baseline="middle fill="${this.textcolor}">${this.text}</text>
+        <text x="50" y="50" text-anchor="middle" dominant-baseline="middle" fill="${this.textcolor}">${this.text}</text>
         </svg>`;
     }
 }
